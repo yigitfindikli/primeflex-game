5d8b270496183de31872f7b229d4cbbad523a57a
yigitfindikli <yigitfindikli@gmail.com>
2021-10-24 18:09:50 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlaygroundComponent } from './playground/playground.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaygroundComponent
   ],
   imports: [
     BrowserModule
